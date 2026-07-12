3189a177e38d49810295cfa497ff0f0b1ce1123e
rockysandeep0909 <rockysandeep0909@gmail.com>
2026-07-12 12:10:42 +0530

    add string program

---
+++ programs/stringexample.js
@@ -0,0 +1,11 @@
+let student ="you guys are doing great , dont stop , keep going";
+let emailid="student@nammaqa.com"
+
+console.log(student);
+console.log(student.length);
+console.log(typeof(student));
+
+
+console.log(student.indexOf("great"))
+
+
