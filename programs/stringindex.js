060b0f0a8cb5bfabb75cc4f319a385ef0de94d63
rockysandeep0909 <rockysandeep0909@gmail.com>
2026-07-18 11:13:33 +0530

    non repeating character

---
+++ programs/stringindex.js
@@ -0,0 +1,18 @@
+
+const str="FIFAWORLDCUP"
+
+for(let char of str){
+  
+    if(str.indexOf(char)==str.lastIndexOf(char)){
+        console.log(char + " is first non repeating character")
+        break;
+    }
+    
+}
+
+
+
+
+
+
+
